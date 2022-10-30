07bb28610b32b67263d1630895fb65ab78235c74
David Hide <d.hide@live.co.uk>
2022-10-30 11:14:06 +0000

    Disable child fields when parent question type is unchecked

--- script.js
+++ script.js
@@ -16,3 +16,25 @@ formFields.forEach(function(field) {
 form.addEventListener('change', function(e) {
     localStorage.setItem(e.target.id, e.target.checked);
 });
+
+const parentFields = document.querySelectorAll('.js-parent-field');
+
+parentFields.forEach(function(parent) {
+    handleDisableChildren(parent); 
+
+    parent.addEventListener('change', function(e) {
+       handleDisableChildren(e.target); 
+    });
+});
+
+function handleDisableChildren(parent) {
+    const children = parent.closest('div').querySelectorAll('.js-child-field');
+        
+    children.forEach(function(child) {
+        if (parent.checked) {
+            child.removeAttribute('disabled');
+        } else {
+            child.setAttribute('disabled', true);
+        }
+    });
+}
